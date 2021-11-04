c834d3929a831f8c45664664897f3ad3764f9fa7
Soren DeHAan <dehaans@carleton.edu>
2021-11-04 10:31:04 -0500

    sentiment analysis pipeline function

---
+++ pipeline_functions/pipeline_sentiment_analysis.py
@@ -0,0 +1,22 @@
+import stanza
+import nltk.sentiment
+import pandas
+
+nltk.download(["vader_lexicon"])
+stanza.download('en', processors='tokenize,sentiment')
+
+
+def getSentiment(dataFrame, inputColumn: str):
+    sentiment_analyzer_stanza = stanza.Pipeline(
+        'en', processors='tokenize,sentiment')
+    sentiment_analyzer_nltk = nltk.sentiment.SentimentIntensityAnalyzer()
+    sentiment_list = []
+    for sentence in dataFrame[inputColumn]:
+        nltk_result = sentiment_analyzer_nltk.polarity_scores(sentence)[
+            'compound']
+        stanza_result = sentiment_analyzer_stanza(
+            sentence).sentences[0].sentiment-1
+        if nltk_result*stanza_result < 0:
+            nltk_result = nltk_result/2
+        sentiment_list.append(nltk_result)
+    return pandas.Series(sentiment_list)
