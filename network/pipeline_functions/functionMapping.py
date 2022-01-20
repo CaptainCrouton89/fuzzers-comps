0512171827cc7e5438e30d8683993dc829c2f73c
CaptainCrouton89 <rhyneer.silas@gmail.com>
2022-01-20 10:31:13 -0600

    MapFunc Class (not going to be used probably)

--- network/pipeline_functions/functionMapping.py
+++ network/pipeline_functions/functionMapping.py
@@ -8,3 +8,19 @@ def example_func(df, column: str, learn=True):
     # perform normalization or other transformation on column here
     return df[column]
 
+class MapFunc():
+    # This isn't being used for anything, but I'm keeping it anyways in case we want to come back.
+    # It would be the parent  class to map functions.
+    def __init__(self, df, column):
+        self.df = df
+        self.column = column
+        self.data = None
+
+    def load_from_file(self, path):
+        pass
+
+    def learn_apply(self):
+        pass
+
+    def apply(self):
+        pass
